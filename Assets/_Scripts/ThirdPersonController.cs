fffc66d8326e16f0ade253fd598fce49dc9c36c5
Markus Lange <Markus@gamedevteacher.com>
2020-07-28 18:10:29 +0200

    Added Music & Tested Filter + ControlerFixes

--- Assets/_Scripts/ThirdPersonController.cs
+++ Assets/_Scripts/ThirdPersonController.cs
@@ -4,25 +4,25 @@ using UnityEngine;
 
 public class ThirdPersonController : MonoBehaviour
 {
-
     // We use Serialize to make the variable appear in the Inspector
     // This allows us to change the variable at runtime 
     [SerializeField] private float _speed = 5f;
-    private Transform Cam;
 
     private float _horizontal = 0f;
     private float _vertical = 0f;
+
+    private float turnSmoothTime = 0.1f;
+    private float turnSmoothVelocity;
+
+    private Transform Cam;
     private Vector3 _moveVector;
-
     private CharacterController _controller;
-
-    [SerializeField] private float turnSmoothTime = 0.1f;
-    float turnSmoothVelocity;
     // Start is called before the first frame update
     void Start ()
     { 
         Cam = Camera.main.transform;
         _controller = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
@@ -44,18 +44,5 @@ public class ThirdPersonController : MonoBehaviour
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             _controller.Move(moveDir * _speed * Time.deltaTime);
         }     
-    }
-
-    void FixedUpdate()
-    {
-        /*// If the Magnitude of the two movement vectors are above 1
-        if (_moveVector.magnitude > 1)
-        {
-            // Remove additional Diagonal speed by normalizing the movement vector (Setting the magnitude back to 1)
-            _moveVector = _moveVector.normalized;
-        }
-        // Set our velocity to be equal to the _moveVector multiplied with our movement speed;
-        _rigidBody.velocity = _moveVector * _speed;
-        */
     }
 }
