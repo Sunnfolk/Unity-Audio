8468275f051d17217857edd9e7dfe2a961eba7c6
Markus Lange <Markus@gamedevteacher.com>
2020-07-29 14:40:31 +0200

    Added Mixer Code

--- Assets/_Scripts/AudioControllerSample.cs
+++ Assets/_Scripts/AudioControllerSample.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 public class AudioControllerSample : MonoBehaviour
 {
     [SerializeField] private AudioSource _source;
-    private AudioClip _clip;
-
+    [SerializeField] private AudioClip _clip;
 
     // Start is called before the first frame update
     void Start()
@@ -17,25 +16,41 @@ public class AudioControllerSample : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // This will play a Clip from its Beginning
-        _source.Play();
-
-        // This Function will Pause the audio clip
-        _source.Pause();
-
-        // This Function will Unpause the audio from where it was paused
-        _source.UnPause();
-
-        // This Function will Stop the audio from playing
-        _source.Stop();
-
-        // This Function will play an audioclip Independant of the AudioSource and can overlap.
-        _source.PlayOneShot(_clip);
-
-        // This you can use to check wether the AudioSource is already playing a song; so that you dont start it over again.
-        if (!_source.isPlaying)
+        if (Input.GetKeyDown(KeyCode.Q))
         {
+            // This will play a Clip from its Beginning.
+            // If a Source is Paused and you call Play(), the source will become unpaused.
+            _source.Play();
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            // This Function will Pause the audio clip
+            _source.Pause();
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            // This Function will Unpause the audio from where it was paused. 
+            // Unlike Play(), it will not create a new playback voice if it is not currently paused.
+            _source.UnPause();
+        }
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            // This Function will Stop the audio from playing
+            _source.Stop();
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            // This Function will play an audioclip Independant of the AudioSource and can overlap.
             _source.PlayOneShot(_clip);
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            // This you can use to check wether the AudioSource is already playing a song; so that you dont start it over again.
+            if (!_source.isPlaying)
+            {
+                _source.PlayOneShot(_clip);
+            }
         }
     }
 }
---
+++ Assets/_Scripts/AudioMixerSample.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioMixerSample : MonoBehaviour
+{
+    [SerializeField] private static AudioMixer _mixer;
+
+    public static void SetSFXVol (float sfxvol)
+    {
+        _mixer.SetFloat("SFXVol", sfxvol);
+    }
+    public static void SetMusicVol(float musicvol )
+    {
+        _mixer.SetFloat("MusicVol", musicvol);
+    }
+    public static void SetMasterVol(float mastervol)
+    {
+        _mixer.SetFloat("MasterVol", mastervol);
+    }
+
+    public void ClearVolume(string name)
+    {
+        _mixer.ClearFloat(name);
+    }
+}
